f6492f7ab15b9faf986ad3313f3e1820c7383d11
akshat113 <akshat113@yahoo.co.in>
2021-05-15 15:58:32 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ header_text = '''
 instructions = '''
     <p><a href="/services">Check out the NLP services!</a></p>\n'''
 home_link = '<p><a href="/">Back</a></p>\n'
-footer_text = '</body>\n</html>'
+footer_text = '<p><b>Made by Akshat Goel, Giovanni Scalzotto, Francesco Forner, Kevin Shah</b></p> </body>\n</html>'
 
 @app.route('/services', methods=['POST', 'GET'])
 def services():
@@ -44,18 +44,6 @@ def services():
         elif form_data['service'] == 'subjectivity': #Giovanni
             sub = subjectivity(form_data['content'])
             return render_template('subjectivity.html', data1 = sub, content = form_data['content'])
-        elif form_data['service'] == 'all_services': #Akshat
-            pol_score = polarity(form_data['content'])
-            freq_results = frequency(form_data['content'])
-            word1sim, word2sim = similarity(form_data['content'], 'good', 'bad')
-            word1sim = pd.DataFrame(word1sim, columns=['Word', 'Similarity score with word1'])
-            word1sim = word1sim.sort_values(by=['Similarity score with word1'], ascending=False)
-            word2sim = pd.DataFrame(word2sim, columns=['Word', 'Similarity score with word2'])
-            word2sim = word2sim.sort_values(by=['Similarity score with word2'], ascending=False)
-            word_freqs, max_freq = word_cloud_generator(form_data['content'])
-            result = pos_tagging(form_data['content'])
-            sub = subjectivity(form_data['content'])
-            return render_template('all_services.html',data1 = sub, content = form_data['content'],data = result,word_freqs = word_freqs, max_freq = max_freq,pol_score = pol_score, freq_results = freq_results,table1=[word1sim.to_html(classes='data')], title1=word1sim.columns.values,table2=[word2sim.to_html(classes='data')], title2=word2sim.columns.values)
         elif form_data['service'] == 'summarization':#Kevin
             valerr = None
             try:
